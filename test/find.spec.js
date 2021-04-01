92071a050ec801d778ba8b2018a1dc676be758d7
Kamata, Ryo <kamataryo@users.noreply.github.com>
2021-04-01 13:17:03 +0900

    通り名をうまく検出できないケースに対応: 町名が正規化を経ている場合 (#102)
    * 通り名を判定するためのロジックの改修。
    例えば「上京区」と「仲之町」に挟まれた住所は、入力した住所が「仲の町」に文字が正規化されているにもかかわらず、データソースの町名が「仲之町」のままなのでうまく通り名が検出できていなかった。なので、検出の際にデータソースの町名も正規化し、正しく検出できるようにした。
    
    https://github.com/geolonia/community-geocoder/issues/84
    
    close #84
    
    * Some fix
    
    * Some fix

--- test/find.spec.js
+++ test/find.spec.js
@@ -314,4 +314,10 @@ describe('Tests for `src/imi-enrichment-address/lib/find.js`.', () => {
     assert.deepEqual('013450001000', res.code)
     assert.deepEqual('', res.tail)
   })
+  // https://github.com/geolonia/community-geocoder/issues/84
+  it('should find the address', () => {
+    const res = find(util.normalize("京都府京都市上京区中長者町通新町西入仲之町276"))
+    assert.deepEqual('261020357000', res.code) // 中長者町通新町西入 を削除
+    assert.deepEqual('276', res.tail)
+  })
 })
